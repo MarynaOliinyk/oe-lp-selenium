aaf7b359665e1f6224d319b7eddc4bcb6310918e
Maryna Oliinyk <oliinyk.maryna@gmail.com>
2017-07-18 15:43:47 -0700

    C20 Incorrect email test fix

--- src/main/java/com/openenglish/pages/RecoveryPage.java
+++ src/main/java/com/openenglish/pages/RecoveryPage.java
@@ -4,18 +4,25 @@ import com.codeborne.selenide.SelenideElement;
 import lombok.Getter;
 import org.openqa.selenium.By;
 
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 
 public class RecoveryPage {
 
     @Getter
     private SelenideElement requestFormTitle = $(By.xpath(".//*[@id='requestForm']/div/h3")),
-                                       email = $(By.id("email")),
-                                        chat = $(By.xpath(".//*[@id='login-support-offline']/a")),
-                               returnToLogin = $(By.xpath(".//*[@id='login2-return']/strong")),
-                                         faq = $(By.id("read-faq")),
-                                      singUp = $(By.id("sign-up")),
-                                   submit = $(By.id("recovery-submit")),
-                                 tooltipText = $(By.xpath(".//div[@class='tooltipster-content']"));
+            email = $(By.id("email")),
+            chat = $(By.xpath(".//*[@id='login-support-offline']/a")),
+            returnToLogin = $(By.xpath(".//*[@id='login2-return']/strong")),
+            faq = $(By.id("read-faq")),
+            singUp = $(By.id("sign-up")),
+            submit = $(By.id("recovery-submit")),
+            tooltipText = $(By.xpath(".//div[@class='tooltipster-content']"));
 
+
+    public void enterEmailAndSubmit(String email) {
+        this.email.shouldBe(visible).val(email);
+        submit.click();
+
+    }
 }
---
+++ src/test/java/com/openenglish/login/reset_password/IncorrectEmailTest.java
@@ -0,0 +1,37 @@
+package com.openenglish.login.reset_password;
+
+import com.openenglish.core.TestBase;
+import com.openenglish.pages.InicioPage;
+import com.openenglish.pages.LoginPage;
+import com.openenglish.pages.RecoveryPage;
+import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.open;
+import static com.openenglish.util.PropertiesCache.getInstance;
+
+
+public class IncorrectEmailTest extends TestBase {
+    private String email = getInstance().getProperty("email");
+    private String password = getInstance().getProperty("password");;
+    private String resetPasswordFormTitle = getInstance().getProperty("reset.password.form.title");
+    private String unregisteredEmail = getInstance().getProperty("unregistered.email");
+    private String toolTipText = getInstance().getProperty("recovery.toolTip.text");
+
+    @Test
+    public void resetPasswordForIncorrectEmailTest() {
+        open("/");
+        LoginPage loginPage = new LoginPage();
+        loginPage.logIn(email, password);
+        InicioPage inicioPage = new InicioPage();
+        inicioPage.getAccountMenu().hover();
+        inicioPage.getLogOutLink().click();
+        loginPage.getPasswordReset().click();
+        RecoveryPage recoveryPage = new RecoveryPage();
+        recoveryPage.getRequestFormTitle().shouldHave(text(resetPasswordFormTitle));
+        recoveryPage.enterEmailAndSubmit(unregisteredEmail);
+        recoveryPage.getTooltipText().shouldBe(visible).shouldHave(text(toolTipText));
+
+    }
+}
