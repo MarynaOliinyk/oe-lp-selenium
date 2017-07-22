16b511af6bc780fb3694b6744a49edfac0cbf2fa
Maryna Oliinyk <oliinyk.maryna@gmail.com>
2017-07-22 20:52:33 +0300

    C 146 verify tiene problemas de ingreso PopUp C 142, c 145 fix

--- src/test/java/com/openenglish/login/fieldsvalidation/C146VerifyTieneProblemasDeIngresoPopUpTest.java
+++ src/test/java/com/openenglish/login/fieldsvalidation/C146VerifyTieneProblemasDeIngresoPopUpTest.java
@@ -1,23 +1,22 @@
 package com.openenglish.login.fieldsvalidation;
 
 import com.openenglish.core.TestBase;
+import com.openenglish.pages.RecoveryPage;
 import org.testng.annotations.Test;
-
 import static com.codeborne.selenide.Condition.attribute;
-import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 
-
-public class С142VerifyContrasenaFieldTest extends TestBase {
-
+public class C146VerifyTieneProblemasDeIngresoPopUpTest extends TestBase{
+//TODO Chat is not enabled
     @Test
-    public void submitInvalidPasswordTest() {
+    public void verifyTieneProblemasDeIngresoPopUpTest (){
         positiveLogIn();
         logOut();
         loginPage.logInAndCheckText(email, invalidPassword, loginTooltipPassword);
         loginPage.logInAndCheckText(email, invalidPassword, loginTooltipPassword);
         loginPage.getSecurityField().shouldHave(attribute("placeholder", securityPlaceHolderText));
+        loginPage.getPopUpResetPassword().shouldBe(visible).click();
+        RecoveryPage recoveryPage = new RecoveryPage();
+        recoveryPage.getChatButton().shouldBe(visible).click();
     }
-
-
 }
