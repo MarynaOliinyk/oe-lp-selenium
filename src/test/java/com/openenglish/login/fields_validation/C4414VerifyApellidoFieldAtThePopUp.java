1a4e696257fb2c5c3a0d553c3dc4c89911ec273d
Anton <kotuyka@gmail.com>
2017-07-23 16:24:31 +0300

    C4414 Verify "Apellido" field at the PopUp

---
+++ src/test/java/com/openenglish/login/fields_validation/C4414VerifyApellidoFieldAtThePopUp.java
@@ -0,0 +1,39 @@
+package com.openenglish.login.fields_validation;
+
+import com.openenglish.core.TestBase;
+import com.openenglish.pages.OpenEnglishPage;
+import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.switchTo;
+
+public class C4414VerifyApellidoFieldAtThePopUp extends TestBase {
+
+    @Test
+    public void appelidoFieldNegativeFlowTest() {
+        open(oeUrl);
+        OpenEnglishPage oePage = new OpenEnglishPage();
+
+        switchTo().frame(oePage.getPopUpIdentifier());
+        oePage.getAppelidoFieldNotification().shouldNotBe(visible);
+        oePage.getComienzaAhoraButton().shouldHave(text(comienzaAhora)).click();
+        oePage.getAppelidoFieldNotification().shouldBe(visible).shouldHave(text(fieldShouldBeCompleted));
+
+        oePage.getAppelidoField().sendKeys(randonNumbers(Integer.parseInt(numberLength)));
+        oePage.getComienzaAhoraButton().shouldHave(text(comienzaAhora)).click();
+        oePage.getAppelidoFieldNotification().shouldBe(visible).shouldHave(text(inCorrectValue));
+        oePage.getAppelidoField().clear();
+
+        oePage.getAppelidoField().sendKeys(specialCharacters);
+        oePage.getComienzaAhoraButton().shouldHave(text(comienzaAhora)).click();
+        oePage.getAppelidoFieldNotification().shouldBe(visible).shouldHave(text(inCorrectValue));
+        oePage.getAppelidoField().clear();
+
+        oePage.getAppelidoField().sendKeys(randCharacters(Integer.parseInt(charQuantity)));
+        oePage.getComienzaAhoraButton().shouldHave(text(comienzaAhora)).click();
+        oePage.getAppelidoFieldNotification().shouldBe(visible).shouldHave(text(lessThenEightyCharacters));
+    }
+
+}
