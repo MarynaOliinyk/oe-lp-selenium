d7e4ab27daf5306808674b12a4218d7822740603
Anton <kotuyka@gmail.com>
2017-07-23 16:11:22 +0300

    C3843 Verify "Nombre" field test at the popUp form done

--- src/main/java/com/openenglish/pages/OpenenglishPage.java
+++ src/main/java/com/openenglish/pages/OpenenglishPage.java
@@ -14,10 +14,10 @@ public class OpenEnglishPage {
             appelidoField = $(By.id("lastname-input")),
             correoElectronField = $(By.id("emailaddress-input")),
             comienzaAhoraButton = $(By.id("submit-button")),
-            comienzaAhoraButtonAtPopUp = $(By.xpath(".//*[@id='pop']//button[@id='submit-button']")),
             nombreFieldNotification = $(By.xpath(".//*[@id='leadForm']/div[1]/div[2]/div/span")),
             appelidoFieldNotification = $(By.xpath(".//*[@id='leadForm']/div[1]/div[3]/div/span")),
             correoElectronFieldNotification = $(By.xpath(".//*[@id='leadForm']/div[1]/div[4]/div/span")),
+            popUpIdentifier = $(By.xpath("//iframe")),
             banerEscButton = $(By.xpath(".//*[@id='popmake-1005']/button"));
 
 
