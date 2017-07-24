dfd9331c1a7988a53c7587cb2bc2f251b4885879
Anton <kotuyka@gmail.com>
2017-07-24 21:12:09 +0300

    Merge branch 'master' into C98_OpenEnglish_Logo_Redirect_To_Inicio_Test
    # Conflicts fix

--- src/main/java/com/openenglish/pages/OpenenglishPage.java
+++ src/main/java/com/openenglish/pages/OpenenglishPage.java
@@ -8,18 +8,13 @@ import static com.codeborne.selenide.Selenide.$;
 
 public class OpenEnglishPage {
 
-    private String registerSectionNotifications = ".//*[@id='%s']/following-sibling::div/span";
-
     @Getter
     private SelenideElement registerForm = $(By.id("leadForm")),
             nombreField = $(By.id("firstname-input")),
             appelidoField = $(By.id("lastname-input")),
-            correoElectronField = $(By.id("emailaddress-input")),
             comienzaAhoraButton = $(By.id("submit-button")),
-            nombreFieldNotification = $(By.xpath(String.format(registerSectionNotifications, "firstname-input"))),
-            appelidoFieldNotification = $(By.xpath(String.format(registerSectionNotifications,"lastname-input"))),
-            correoElectronFieldNotification = $(By.xpath(String.format(registerSectionNotifications, "emailaddress-input"))),
-            popUpIdentifier = $(By.xpath("//iframe")),
+            nombreFieldNotification = $(By.xpath(".//*[@id='leadForm']/div[1]/div[2]/div/span")),
+            appelidoFieldNotification = $(By.xpath(".//*[@id='leadForm']/div[1]/div[3]/div/span")),
             banerEscButton = $(By.xpath(".//*[@id='popmake-1005']/button"));
 
 
