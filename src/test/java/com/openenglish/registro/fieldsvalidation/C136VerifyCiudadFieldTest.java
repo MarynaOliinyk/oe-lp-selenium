e09fa205439cf2fe8b9b633f8eeead26b9fe622d
Anton <kotuyka@gmail.com>
2017-07-25 01:00:46 +0300

    C136 Verify "Ciudad" field contains bug: LPTRIAGE-294 - No attachment in C136 Verify "Ciudad" field (https://openenglish.jira.com/browse/LPTRIAGE-294)

---
+++ src/test/java/com/openenglish/registro/fieldsvalidation/C136VerifyCiudadFieldTest.java
@@ -0,0 +1,13 @@
+package com.openenglish.registro.fieldsvalidation;
+
+import org.testng.annotations.Test;
+
+public class C136VerifyCiudadFieldTest {
+
+    // TODO bug LPTRIAGE-294 - No attachment in C136 Verify "Ciudad" field (https://openenglish.jira.com/browse/LPTRIAGE-294)
+    @Test
+    public void ciudadFieldVerifyTest() {
+
+    }
+
+}
