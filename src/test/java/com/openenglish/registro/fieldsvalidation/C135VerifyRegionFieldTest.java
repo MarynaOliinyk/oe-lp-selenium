ae12bfa276a899dc513f2c844101410844367f34
Anton <kotuyka@gmail.com>
2017-07-25 00:57:40 +0300

    C135 Verify "País" field contains bug: LPTRIAGE-293 - No attachment in C135 Verify "Región" field (https://openenglish.jira.com/browse/LPTRIAGE-293)

---
+++ src/test/java/com/openenglish/registro/fieldsvalidation/C135VerifyRegionFieldTest.java
@@ -0,0 +1,13 @@
+package com.openenglish.registro.fieldsvalidation;
+
+import org.testng.annotations.Test;
+
+public class C135VerifyRegionFieldTest {
+
+    // TODO bug LPTRIAGE-293 - No attachment in C135 Verify "Región" field (https://openenglish.jira.com/browse/LPTRIAGE-293)
+    @Test
+    public void regionFieldVerifyTest() {
+
+    }
+
+}
