4ab2fcc5bb5cc5bffd3cf54bbb6d0e5781e943c6
Anton <kotuyka@gmail.com>
2017-07-25 01:04:59 +0300

    C137 Verify "Teléfono" field contains bug: LPTRIAGE-296 - No attachment in C137 Verify "Teléfono" field (https://openenglish.jira.com/browse/LPTRIAGE-296)
    C135 and C136 code fix

--- src/test/java/com/openenglish/registro/fieldsvalidation/C136VerifyCiudadFieldTest.java
+++ src/test/java/com/openenglish/registro/fieldsvalidation/C136VerifyCiudadFieldTest.java
@@ -1,8 +1,9 @@
 package com.openenglish.registro.fieldsvalidation;
 
+import com.openenglish.core.TestBase;
 import org.testng.annotations.Test;
 
-public class C136VerifyCiudadFieldTest {
+public class C136VerifyCiudadFieldTest extends TestBase {
 
     // TODO bug LPTRIAGE-294 - No attachment in C136 Verify "Ciudad" field (https://openenglish.jira.com/browse/LPTRIAGE-294)
     @Test
