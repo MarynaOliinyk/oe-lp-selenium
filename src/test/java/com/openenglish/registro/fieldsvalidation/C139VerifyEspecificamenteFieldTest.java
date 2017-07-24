251ba6586fd8a40c4a89dad9a73753bdd3c46a4c
Anton <kotuyka@gmail.com>
2017-07-24 23:13:03 +0300

    C139 Verify "Especificamente" field contains bug. LPTRIAGE-291 - There is no "¿Cómo supo de nosotros?" field (https://openenglish.jira.com/secure/RapidBoard.jspa?rapidView=316&projectKey=LPTRIAGE&view=planning&selectedIssue=LPTRIAGE-291&quickFilter=1253)

---
+++ src/test/java/com/openenglish/registro/fieldsvalidation/C139VerifyEspecificamenteFieldTest.java
@@ -0,0 +1,14 @@
+package com.openenglish.registro.fieldsvalidation;
+
+import com.openenglish.core.TestBase;
+import org.testng.annotations.Test;
+
+public class C139VerifyEspecificamenteFieldTest extends TestBase {
+
+    // TODO bug LPTRIAGE-291 - There is no "¿Cómo supo de nosotros?" field (https://openenglish.jira.com/secure/RapidBoard.jspa?rapidView=316&projectKey=LPTRIAGE&view=planning&selectedIssue=LPTRIAGE-291&quickFilter=1253)
+    @Test
+    public void especificamenteFieldVerifyTest() {
+
+    }
+
+}
