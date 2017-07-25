01f72d2129206cb9fd4c800679c589cd59b488b1
Anton <kotuyka@gmail.com>
2017-07-26 00:42:30 +0300

    C6 Verify "Inicio" page content - in progress

--- src/main/java/com/openenglish/pages/InicioPage.java
+++ src/main/java/com/openenglish/pages/InicioPage.java
@@ -5,18 +5,32 @@ import lombok.Getter;
 import lombok.experimental.Accessors;
 import org.openqa.selenium.By;
 
+import java.util.ArrayList;
+
 import static com.codeborne.selenide.Selenide.$;
 
 @Accessors(chain = true)
 public class InicioPage {
     @Getter
-    private SelenideElement userName = $(By.xpath(".//*[@id='student-info']//div[@class='display-name']")),
+    private SelenideElement userInfoElement = $(By.xpath(".//*[@id='student-info']//div[@class='student-level']")),
+            userName = $(By.xpath(".//*[@id='student-info']//div[@class='display-name']")),
+            userPhoto = $(By.xpath(".//*[@id='student-info']//div[@class='photo-frame']")),
             accountMenu = $(By.id("account-menu-nav")),
             mensajesCuentaSuboptionButton = $(By.id("messages")),
             preferenciasCuentaSuboptionButton = $(By.xpath(".//*[@id='account-menu']//li[@data-name='preferences']/a")),
             inicioLink = $(By.id("nav-home")),
             progresoLink = $(By.id("nav-progress")),
             cuentaSuboptions = $(By.xpath(".//*[@id='account-menu']/div[@class='drop-slide-wrapper']/ul")),
+            headingElement = $(By.id("student-info")),
+            planDeEstudioElement= $(By.xpath(".//*[@id='student-info']//div[@class='grid_6']//div")),
+            planDeEstudioText= $(By.xpath(".//*[@id='student-info']//div[@class='grid_6']//span")),
             logOutLink = $(By.xpath(".//*[@id='account-menu']//a[@href='/j_spring_security_logout']")),
-            oeLogo = $(By.xpath(".//*[@id='header']//a[@href='/index.html']"));
+            oeLogo = $(By.xpath(".//*[@id='header']//a[@href='/index.html']")),
+            videoBlock = $(By.id("main")),
+            carouselVideoBlock = $(By.xpath(".//*[@id='main']//div[@class='carousel']")),
+            lessonVideoBlock = $(By.xpath(".//*[@id='main']//div[@class='lesson']"));
+
+    @Getter
+    private By practiceVideosBlocks = By.xpath(".//*[@id='main']//div[@data-show-video='overlay']");
+
 }
---
+++ src/test/java/com/openenglish/inicio/C6VerifyInicioContentTest.java
@@ -0,0 +1,46 @@
+package com.openenglish.inicio;
+
+import com.openenglish.core.TestBase;
+import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Condition.attribute;
+import static com.codeborne.selenide.Condition.empty;
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$$;
+import static org.testng.Assert.assertTrue;
+import static org.testng.AssertJUnit.assertFalse;
+
+public class C6VerifyInicioContentTest extends TestBase {
+
+    @Test
+    public void verifyInicioPageContentTest() {
+        loginPage.cookieBannerVisibility();
+        loginPage.getPassword().shouldBe(visible)
+                .shouldHave(attribute("type", "password"));
+        loginPage.logIn(email, correctPassword);
+        inicioPage.getInicioLink().shouldBe(visible).shouldNotBe(empty)
+                .shouldHave(exactText(inicioLink));
+        inicioPage.getUserName().shouldBe(visible).shouldNotBe(empty)
+                .shouldHave(text(userName));
+        inicioPage.getHeadingElement().shouldBe(visible).shouldNotBe(empty)
+                .shouldHave(attribute("class", "container_12"));
+        inicioPage.getPlanDeEstudioElement().shouldBe(visible).shouldNotBe(empty)
+                .shouldHave(attribute("class", "plan-text"));
+        inicioPage.getPlanDeEstudioText().shouldBe(visible).shouldNotBe(empty)
+                .shouldHave(exactText("Plan de estudio"));
+        inicioPage.getUserInfoElement().shouldBe(visible).shouldNotBe(empty)
+                .shouldHave(attribute("class", "student-level"));
+        inicioPage.getUserPhoto().shouldBe(visible).shouldNotBe(empty)
+                .shouldHave(attribute("class", "photo-frame"));
+        inicioPage.getVideoBlock().shouldBe(visible).shouldNotBe(empty);
+        inicioPage.getCarouselVideoBlock().shouldBe(visible).shouldNotBe(empty);
+        inicioPage.getLessonVideoBlock().shouldBe(visible).shouldNotBe(empty);
+        assertTrue($$ (inicioPage.getPracticeVideosBlocks()).size()>=0);
+        assertFalse($$ (inicioPage.getPracticeVideosBlocks()).isEmpty());
+
+// TODO finish it
+    }
+
+}
