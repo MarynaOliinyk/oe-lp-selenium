78bd1a9a9cebf7a69f2acb06c0822f21e3913a95
Maryna Oliinyk <oliinyk.maryna@gmail.com>
2017-07-26 15:36:30 +0300

    C 100 verify progreso menu links Scenario 1 resolve conflicts

--- src/main/java/com/openenglish/pages/ProfilePage.java
+++ src/main/java/com/openenglish/pages/ProfilePage.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.By;
 import static com.codeborne.selenide.Selenide.$;
 
 public class ProfilePage {
+
     @Getter
     private SelenideElement headerText = $(By.xpath(".//*[@class='grid_3']/h1"));
+
 }
