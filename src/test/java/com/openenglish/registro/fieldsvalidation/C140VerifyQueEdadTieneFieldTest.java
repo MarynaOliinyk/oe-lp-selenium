884616805ee913d49e09d7796c9e36fa92be10a3
Anton <kotuyka@gmail.com>
2017-07-28 00:51:15 +0300

    links fix on a Jira issues.

--- src/test/java/com/openenglish/registro/fieldsvalidation/C140VerifyQueEdadTieneFieldTest.java
+++ src/test/java/com/openenglish/registro/fieldsvalidation/C140VerifyQueEdadTieneFieldTest.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 
 public class C140VerifyQueEdadTieneFieldTest extends DriverBase {
 
-    // TODO bug LPTRIAGE-292 - Wrong ages in "¿Qué edad tiene?" field in C140 (https://openenglish.jira.com/browse/LPTRIAGE-297)
+    // TODO bug LPTRIAGE-297 - Wrong ages in "¿Qué edad tiene?" field in C140 (https://openenglish.jira.com/browse/LPTRIAGE-297)
     @Test(enabled = false)
     public void queEdadTieneFieldVerifyTest() {
 
