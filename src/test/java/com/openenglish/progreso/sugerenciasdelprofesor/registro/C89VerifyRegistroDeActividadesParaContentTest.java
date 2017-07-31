5fd9f172414b65af26fb2a6d146beb33d0b98335
Anton <kotuyka@gmail.com>
2017-08-01 02:28:37 +0300

    Qaa-91/C89-Verify-Registro-de-actividades-para-content: refactoring # Conflicts fix.

--- src/test/java/com/openenglish/progreso/sugerenciasdelprofesor/registro/C89VerifyRegistroDeActividadesParaContentTest.java
+++ src/test/java/com/openenglish/progreso/sugerenciasdelprofesor/registro/C89VerifyRegistroDeActividadesParaContentTest.java
@@ -3,23 +3,24 @@ package com.openenglish.progreso.sugerenciasdelprofesor.registro;
 import com.openenglish.core.DriverBase;
 import com.openenglish.pages.AbstractPage;
 import com.openenglish.pages.HistoryPage;
+import com.openenglish.pages.LoginPage;
 import org.testng.annotations.Test;
 
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.open;
-import static com.openenglish.core.TestData.General.lpUrl;
+import static com.openenglish.core.TestData.General.LP_URL;
+import static com.openenglish.core.TestData.History.HISTORY_HEADER_TEXT;
 import static com.openenglish.core.TestData.History.historyHeaderLinkText;
-import static com.openenglish.core.TestData.History.historyHeaderText;
 import static com.openenglish.core.TestData.History.historyTableHeaderDescripcionText;
 import static com.openenglish.core.TestData.History.historyTableHeaderFechaText;
 import static com.openenglish.core.TestData.History.historyTableHeaderResultsText;
 import static com.openenglish.core.TestData.History.historyTableHeaderTipoText;
 import static com.openenglish.core.TestData.History.historyTableHeaderTituloText;
 import static com.openenglish.core.TestData.History.historyUrlEndsWith;
-import static com.openenglish.core.TestData.Login.correctPassword;
-import static com.openenglish.core.TestData.Login.registeredEmail;
-import static com.openenglish.core.TestData.Login.userName;
+import static com.openenglish.core.TestData.Login.CORRECT_PASSWORD;
+import static com.openenglish.core.TestData.Login.REGISTERED_EMAIL;
+import static com.openenglish.core.TestData.Login.USER_NAME;
 import static org.testng.Assert.assertEquals;
 
 public class C89VerifyRegistroDeActividadesParaContentTest extends DriverBase {
@@ -28,10 +29,11 @@ public class C89VerifyRegistroDeActividadesParaContentTest extends DriverBase {
 
     @Test
     public void registroDeActividadesParaContentVerifyTest() {
-        open(lpUrl);
+        open(LP_URL);
 
-        page.loginPage.cookieBannerVisibility();
-        page.loginPage.logIn(registeredEmail, correctPassword);
+        LoginPage loginPage = new LoginPage();
+        loginPage.cookieBannerVisibility();
+        loginPage.logIn(REGISTERED_EMAIL, CORRECT_PASSWORD);
 
         page.inicioPage.getProgresoLink().hover();
         page.inicioPage.getRegistroLink().click();
@@ -40,10 +42,10 @@ public class C89VerifyRegistroDeActividadesParaContentTest extends DriverBase {
         historyPage.getHeaderBlock().shouldBe(visible);
 
         historyPage.getHeaderRegistroDeActividadesText().shouldBe(visible)
-                .shouldHave(exactText(historyHeaderText + " " + userName));
+                .shouldHave(exactText(HISTORY_HEADER_TEXT + " " + USER_NAME));
         historyPage.getHeaderVerIndiceDelCursoLink().shouldBe(visible)
                 .shouldHave(exactText(historyHeaderLinkText));
-        assertEquals(historyPage.getHeaderVerIndiceDelCursoLink().getAttribute("href"), lpUrl + historyUrlEndsWith);
+        assertEquals(historyPage.getHeaderVerIndiceDelCursoLink().getAttribute("href"), LP_URL + historyUrlEndsWith);
 
         historyPage.getPaginationBlock().shouldBe(visible);
 
