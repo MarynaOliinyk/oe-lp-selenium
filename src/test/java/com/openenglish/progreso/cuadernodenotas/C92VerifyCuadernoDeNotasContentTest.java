3b529102a0a2fad1a02babe8533bdb3476ed66e7
Maryna Oliinyk <oliinyk.maryna@gmail.com>
2017-07-31 23:45:29 +0300

    C92 verify cuaderno de notas content scenario 2

--- src/test/java/com/openenglish/progreso/cuadernodenotas/C92VerifyCuadernoDeNotasContentTest.java
+++ src/test/java/com/openenglish/progreso/cuadernodenotas/C92VerifyCuadernoDeNotasContentTest.java
@@ -14,6 +14,9 @@ import static com.openenglish.core.TestData.Inicio.inicioLink;
 import static com.openenglish.core.TestData.Login.EMAIL_WITHOUT_NOTES;
 import static com.openenglish.core.TestData.Login.PASSWORD_WITHOUT_NOTES;
 import static com.openenglish.core.TestData.Login.USER_NAME_WITHOUT_NOTES;
+import static com.openenglish.core.TestData.Login.correctPassword;
+import static com.openenglish.core.TestData.Login.registeredEmail;
+import static com.openenglish.core.TestData.Login.userName;
 import static com.openenglish.core.TestData.MyNotebook.myNotebookHeaderText;
 import static com.openenglish.core.TestData.MyNotebook.myNotebookVerElIndiceDelCursoLink;
 import static com.openenglish.core.TestData.MyNotebook.myNotebookVerElIndiceDelCursoText;
@@ -42,5 +45,33 @@ public class C92VerifyCuadernoDeNotasContentTest extends DriverBase {
                 .contains(myNotebookVerElIndiceDelCursoLink));
         myNotebookPage.getTextIfNotNotes().should(visible, text(myNotebooktextIfNotNotes));
 
+    }
+
+    @Test
+    public void verifyCuadernoDeNotasContentTestS2() {
+        open(lpUrl);
+        page.loginPage.cookieBannerVisibility();
+        page.loginPage.getPassword().shouldBe(visible)
+                .shouldHave(attribute("type", "password"));
+        page.loginPage.logIn(registeredEmail, correctPassword);
+        page.inicioPage.getInicioLink().shouldHave(text(inicioLink));
+        //TODO BUG NSB-65  https://openenglish.jira.com/browse/NSB-65
+//        page.inicioPage.getUserName().shouldHave(text(userNickName));
+        page.inicioPage.getProgresoLink().click();
+        page.inicioPage.getCuadernoDeNotasLink().click();
+        MyNotebookPage myNotebookPage = new MyNotebookPage();
+        myNotebookPage.getHeaderText().shouldBe(visible).shouldHave(text(myNotebookHeaderText + " " + userName));
+        assertTrue(myNotebookPage.getVerElIndiceDelCursoLink().should(text(myNotebookVerElIndiceDelCursoText)).getAttribute("href")
+                .contains(myNotebookVerElIndiceDelCursoLink));
+        myNotebookPage.getPagination().should(visible);
+        myNotebookPage.getNotes().should(visible);
+        myNotebookPage.getFechaColumn().should(visible);
+        myNotebookPage.getDateAndHourOfNoteCreationData().should(visible);
+        myNotebookPage.getUnitAndLessonColumn().should(visible);
+        myNotebookPage.getUnitAndLessonNumberData().should(visible);
+        myNotebookPage.getTitleLessonColumn().should(visible);
+        myNotebookPage.getLessonTitleData().should(visible);
+        myNotebookPage.getNoteColumn().should(visible);
+        myNotebookPage.getNoteDescription().should(visible);
     }
 }
