2a465cb67b6d71243cbcc743078f1b34c81e3bcb
Anton <kotuyka@gmail.com>
2017-08-01 23:31:25 +0300

    refactoring variables for C92 Verify Cuaderno De Notas Content Test

--- src/test/java/com/openenglish/progreso/cuadernodenotas/C92VerifyCuadernoDeNotasContentTest.java
+++ src/test/java/com/openenglish/progreso/cuadernodenotas/C92VerifyCuadernoDeNotasContentTest.java
@@ -9,15 +9,14 @@ import static com.codeborne.selenide.Condition.attribute;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.open;
-import static com.openenglish.core.TestData.General.lpUrl;
-import static com.openenglish.core.TestData.Inicio.inicioLink;
+import static com.openenglish.core.TestData.General.LP_URL;
+import static com.openenglish.core.TestData.Inicio.INICIO_LINK;
+import static com.openenglish.core.TestData.Login.CORRECT_PASSWORD;
 import static com.openenglish.core.TestData.Login.EMAIL_WITHOUT_NOTES;
 import static com.openenglish.core.TestData.Login.PASSWORD_WITHOUT_NOTES;
+import static com.openenglish.core.TestData.Login.REGISTERED_EMAIL;
 import static com.openenglish.core.TestData.Login.USER_NAME_WITHOUT_NOTES;
-import static com.openenglish.core.TestData.Login.correctPassword;
-import static com.openenglish.core.TestData.Login.registeredEmail;
-import static com.openenglish.core.TestData.Login.userName;
-import static com.openenglish.core.TestData.MyNotebook.myNotebookHeaderText;
+import static com.openenglish.core.TestData.MyNotebook.MY_NOTEBOOK_HEADER_TEXT;
 import static com.openenglish.core.TestData.MyNotebook.MY_NOTEBOOK_VER_EL_INDICE_DEL_CURSO_LINK;
 import static com.openenglish.core.TestData.MyNotebook.MY_NOTEBOOK_VER_EL_INDICE_DEL_CURSO_TEXT;
 import static com.openenglish.core.TestData.MyNotebook.MY_NOTEBOOKTEXT_IF_NOT_NOTES;
@@ -29,18 +28,18 @@ public class C92VerifyCuadernoDeNotasContentTest extends DriverBase {
 
     @Test
     public void verifyCuadernoDeNotasContentTestS1() {
-        open(lpUrl);
+        open(LP_URL);
         page.loginPage.cookieBannerVisibility();
         page.loginPage.getPassword().shouldBe(visible)
                 .shouldHave(attribute("type", "password"));
         page.loginPage.logIn(EMAIL_WITHOUT_NOTES, PASSWORD_WITHOUT_NOTES);
-        page.inicioPage.getInicioLink().shouldHave(text(inicioLink));
+        page.inicioPage.getInicioLink().shouldHave(text(INICIO_LINK));
         //TODO BUG NSB-65  https://openenglish.jira.com/browse/NSB-65
 //        page.inicioPage.getUserName().shouldHave(text(USER_NICK_NAME_WITHOUT_NOTES));
         page.inicioPage.getProgresoLink().click();
         page.inicioPage.getCuadernoDeNotasLink().click();
         MyNotebookPage myNotebookPage = new MyNotebookPage();
-        myNotebookPage.getHeaderText().shouldBe(visible).shouldHave(text(myNotebookHeaderText + " " + USER_NAME_WITHOUT_NOTES));
+        myNotebookPage.getHeaderText().shouldBe(visible).shouldHave(text(MY_NOTEBOOK_HEADER_TEXT + " " + USER_NAME_WITHOUT_NOTES));
         assertTrue(myNotebookPage.getVerElIndiceDelCursoLink().should(text(MY_NOTEBOOK_VER_EL_INDICE_DEL_CURSO_TEXT)).getAttribute("href")
                 .contains(MY_NOTEBOOK_VER_EL_INDICE_DEL_CURSO_LINK));
         myNotebookPage.getTextIfNotNotes().should(visible, text(MY_NOTEBOOKTEXT_IF_NOT_NOTES));
@@ -49,18 +48,18 @@ public class C92VerifyCuadernoDeNotasContentTest extends DriverBase {
 
     @Test
     public void verifyCuadernoDeNotasContentTestS2() {
-        open(lpUrl);
+        open(LP_URL);
         page.loginPage.cookieBannerVisibility();
         page.loginPage.getPassword().shouldBe(visible)
                 .shouldHave(attribute("type", "password"));
-        page.loginPage.logIn(registeredEmail, correctPassword);
-        page.inicioPage.getInicioLink().shouldHave(text(inicioLink));
+        page.loginPage.logIn(REGISTERED_EMAIL, CORRECT_PASSWORD);
+        page.inicioPage.getInicioLink().shouldHave(text(INICIO_LINK));
         //TODO BUG NSB-65  https://openenglish.jira.com/browse/NSB-65
 //        page.inicioPage.getUserName().shouldHave(text(userNickName));
         page.inicioPage.getProgresoLink().click();
         page.inicioPage.getCuadernoDeNotasLink().click();
         MyNotebookPage myNotebookPage = new MyNotebookPage();
-        myNotebookPage.getHeaderText().shouldBe(visible).shouldHave(text(myNotebookHeaderText + " " + userName));
+        myNotebookPage.getHeaderText().shouldBe(visible).shouldHave(text(MY_NOTEBOOK_HEADER_TEXT + " " + USER_NAME_WITHOUT_NOTES));
         assertTrue(myNotebookPage.getVerElIndiceDelCursoLink().should(text(MY_NOTEBOOK_VER_EL_INDICE_DEL_CURSO_TEXT)).getAttribute("href")
                 .contains(MY_NOTEBOOK_VER_EL_INDICE_DEL_CURSO_LINK));
         myNotebookPage.getPagination().should(visible);
