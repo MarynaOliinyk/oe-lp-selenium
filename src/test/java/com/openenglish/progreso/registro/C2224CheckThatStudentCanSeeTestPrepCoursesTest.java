b1b7164b04b9997655e4fa2a9bbfd4a119a28018
Anton <kotuyka@gmail.com>
2017-08-03 23:54:08 +0300

    moved test from package (Master->)Progreso->Sugerencias del profesor->Registro to the package (Master->)Progreso->Registro

--- src/test/java/com/openenglish/progreso/registro/C2224CheckThatStudentCanSeeTestPrepCoursesTest.java
+++ src/test/java/com/openenglish/progreso/registro/C2224CheckThatStudentCanSeeTestPrepCoursesTest.java
@@ -1,28 +1,24 @@
-package com.openenglish.progreso.sugerenciasdelprofesor.registro;
+package com.openenglish.progreso.registro;
 
 import com.openenglish.core.DriverBase;
 import com.openenglish.pages.HistoryPage;
 import com.openenglish.pages.InicioPage;
 import com.openenglish.pages.LoginPage;
-import com.openenglish.pages.VideoPopUp;
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$$;
+
 import static com.codeborne.selenide.Selenide.open;
-import static com.codeborne.selenide.Selenide.switchTo;
 import static com.openenglish.core.TestData.General.LP_URL;
-import static com.openenglish.core.TestData.History.HISTORY_TABLE_RESULTS_REPRODUCIR_TEXT;
 import static com.openenglish.core.TestData.History.HISTORY_TIPO_TEXT;
 import static com.openenglish.core.TestData.Login.CORRECT_PASSWORD;
 import static com.openenglish.core.TestData.Login.REGISTERED_EMAIL;
-import static com.openenglish.core.TestData.Selenide.WAIT_UNTIL;
-import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
 
-public class C2225CheckThatUserCanReplayTestPrepCoursesTest extends DriverBase {
+public class C2224CheckThatStudentCanSeeTestPrepCoursesTest extends DriverBase {
 
     @Test
     public void checkThatStudentCanSeeTestPrepCoursesTest() {
@@ -44,13 +40,6 @@ public class C2225CheckThatUserCanReplayTestPrepCoursesTest extends DriverBase {
         historyPage.getSearchTypePractice().shouldBe(visible).click();
         historyPage.getAddTypeButton().click();
         assertTrue(historyPage.isAllTheListContainsText($$(By.xpath(historyPage.getTipoTableEllements())), HISTORY_TIPO_TEXT));
-        assertEquals(historyPage.getResultsAndActionsTableAllElements().getText(), HISTORY_TABLE_RESULTS_REPRODUCIR_TEXT);
-        historyPage.getResultsAndActionsTableAllElements().click();
-
-        VideoPopUp videoPopUp = new VideoPopUp();
-        switchTo().frame(videoPopUp.getVideoIframe());
-        videoPopUp.getPausaButton().waitUntil(visible, WAIT_UNTIL).click();
-        videoPopUp.getReproducirButton().waitUntil(visible, WAIT_UNTIL);
+        assertFalse($$(By.xpath(historyPage.getTituloTableEllements())).isEmpty());
     }
-
 }
