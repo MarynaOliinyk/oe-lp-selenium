bfc6f105dd8607b2d24406dae93d00032f436781
Anton <kotuyka@gmail.com>
2017-08-03 23:47:02 +0300

    C2225 Check That User Can Replay Test Prep Courses Test - Done

--- src/main/java/com/openenglish/pages/VideoPopUp.java
+++ src/main/java/com/openenglish/pages/VideoPopUp.java
@@ -9,5 +9,7 @@ import static com.codeborne.selenide.Selenide.$;
 @Getter
 public class VideoPopUp {
 
-    private SelenideElement reproducirButton = $(By.xpath(".//span[@class='play-pause-label paused']"));
+    private SelenideElement videoIframe = $(By.xpath(".//div[@id='ec-player']//iframe")),
+            pausaButton = $(By.xpath(".//span[@class='play-pause-label playing']")),
+            reproducirButton = $(By.xpath(".//span[@class='play-pause-label paused']"));
 }
