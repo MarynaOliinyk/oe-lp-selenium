7b950c2ea497622521fa0e2a619c62e60fa3aec2
88SergiiOliinyk <sergii.oliinyk@thomascookonline.com>
2017-08-14 22:54:28 +0300

    [QAA-38] C128 Verify "Teléfono" field

--- src/test/java/com/openenglish/registro/C128VerifyTelefonoFieldTest.java
+++ src/test/java/com/openenglish/registro/C128VerifyTelefonoFieldTest.java
@@ -11,9 +11,9 @@ import static com.codeborne.selenide.Selenide.switchTo;
 import static com.openenglish.core.TestData.General.OE_URL;
 import static com.openenglish.core.TestData.Register.MOBILE_AREA_CODES_MESSAGE;
 import static com.openenglish.core.TestData.Register.MOBILE_AREA_CODES_PATTERN;
+import static com.openenglish.core.TestData.Register.MOBILE_SECTION_ONE_INCORRECT;
 import static com.openenglish.core.TestData.Register.MOBILE_SECTION_THREE;
 import static com.openenglish.core.TestData.Register.MOBILE_SECTION_TWO;
-import static com.openenglish.core.TestData.Register.NUMBER_LENGTH;
 import static org.testng.AssertJUnit.assertTrue;
 
 public class C128VerifyTelefonoFieldTest extends DriverBase {
@@ -24,7 +24,7 @@ public class C128VerifyTelefonoFieldTest extends DriverBase {
         switchTo().frame(regForm.getPopUpIdentifier());
         assertTrue(regForm.getCellular().getAttribute("class").contains("active"));
         regForm.getFijo().should(visible);
-        regForm.getMobileSectionOne().val(NUMBER_LENGTH);
+        regForm.getMobileSectionOne().val(MOBILE_SECTION_ONE_INCORRECT);
         regForm.getMobileSectionTwo().val(MOBILE_SECTION_TWO);
         regForm.getMobileSectionThree().val(MOBILE_SECTION_THREE);
         regForm.getComienzaAhoraButton().click();
