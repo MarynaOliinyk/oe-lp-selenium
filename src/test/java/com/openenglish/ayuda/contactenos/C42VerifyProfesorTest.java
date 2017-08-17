dbe9aa0c8f06443ad74ec2ba8595fc9ed8d52c5d
Anton <kotuyka@gmail.com>
2017-08-18 01:33:02 +0300

    [QAA-130] C42 Verify "Profesor" test DONE

---
+++ src/test/java/com/openenglish/ayuda/contactenos/C42VerifyProfesorTest.java
@@ -0,0 +1,34 @@
+package com.openenglish.ayuda.contactenos;
+
+import com.openenglish.core.DriverBase;
+import com.openenglish.pages.InicioPage;
+import com.openenglish.pages.LoginPage;
+import com.openenglish.pages.TalkNowPage;
+import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.open;
+import static com.openenglish.core.TestData.General.LP_URL;
+import static com.openenglish.core.TestData.Login.CORRECT_PASSWORD;
+import static com.openenglish.core.TestData.Login.REGISTERED_EMAIL;
+import static com.openenglish.core.TestData.TalkNow.TALK_NOW_PROFESOR_HOVER_TEXT;
+
+public class C42VerifyProfesorTest extends DriverBase {
+
+    @Test
+    public void verifyProfesorTest() {
+        open(LP_URL);
+        LoginPage lp = new LoginPage();
+        lp.cookieBannerVisibility();
+        lp.logIn(REGISTERED_EMAIL, CORRECT_PASSWORD);
+
+        InicioPage ip = new InicioPage();
+        ip.getAyudaDropDown().hover();
+        ip.getContactenos().click();
+
+        TalkNowPage talkNowPage = new TalkNowPage();
+        talkNowPage.getProfesorImg().hover();
+        talkNowPage.getProfesorImgHoverText().shouldBe(visible, exactText(TALK_NOW_PROFESOR_HOVER_TEXT));
+    }
+}
