c316a3466565a09a43f3ef6add0dc2966e09f0c0
Anton <kotuyka@gmail.com>
2017-08-22 00:40:24 +0300

    [QAA-132] C109 Verify "Enviar una pregunta" content test Done

---
+++ src/test/java/com/openenglish/ayuda/contactenos/C109VerifyEnviarUnaPreguntaContentTextTest.java
@@ -0,0 +1,62 @@
+package com.openenglish.ayuda.contactenos;
+
+import com.openenglish.core.DriverBase;
+import com.openenglish.pages.InicioPage;
+import com.openenglish.pages.LoginPage;
+import com.openenglish.pages.TalkNowPage;
+import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Condition.attribute;
+import static com.codeborne.selenide.Condition.empty;
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.open;
+import static com.openenglish.core.Attribute.CLASS;
+import static com.openenglish.core.Attribute.PLACEHOLDER;
+import static com.openenglish.core.Attribute.Value.TalkNowPage.ORANGE_BTN;
+import static com.openenglish.core.Attribute.Value.TalkNowPage.ORANGE_BTN_CLOSE;
+import static com.openenglish.core.Attribute.Value.TalkNowPage.ORANGE_BTN_DISABLED;
+import static com.openenglish.core.TestData.General.LP_URL;
+import static com.openenglish.core.TestData.Lessons.ALLERT_CERRAR_TEXT;
+import static com.openenglish.core.TestData.Lessons.ALLERT_HEADER_TEXT;
+import static com.openenglish.core.TestData.Lessons.ASK_TEACHER_SUBMIT_TEXT;
+import static com.openenglish.core.TestData.Lessons.SOME_TEXT_FOR_TEST;
+import static com.openenglish.core.TestData.Login.CORRECT_PASSWORD;
+import static com.openenglish.core.TestData.Login.REGISTERED_EMAIL;
+import static com.openenglish.core.TestData.TalkNow.ENVIAR_UNA_PREGUNTA_TEXT;
+import static com.openenglish.core.TestData.TalkNow.PREGUNTE_A_UN_PROFESOR_TEXT;
+import static com.openenglish.core.TestData.TalkNow.PREGUNTE_POPUP_INSTRUCTIONS_TEXT;
+import static com.openenglish.core.TestData.TalkNow.PREGUNTE_POPUP_MESSAGE_PLACEHOLDER_TEXT;
+import static com.openenglish.core.TestData.TalkNow.TALK_NOW_HEADER_TEXT;
+
+public class C109VerifyEnviarUnaPreguntaContentTextTest extends DriverBase {
+
+    @Test
+    public void verifyEnviarUnaPreguntaContentTextTest() {
+        open(LP_URL);
+        LoginPage lp = new LoginPage();
+        lp.cookieBannerVisibility();
+        lp.logIn(REGISTERED_EMAIL, CORRECT_PASSWORD);
+        InicioPage ip = new InicioPage();
+        ip.getAyudaDropDown().hover();
+        ip.getContactenos().click();
+
+        TalkNowPage talkNowPage = new TalkNowPage();
+        talkNowPage.getEnviarUnaPreguntaLink().shouldBe(visible, exactText(ENVIAR_UNA_PREGUNTA_TEXT)).click();
+        talkNowPage.getPopupPreguntaTitle().shouldBe(visible, exactText(PREGUNTE_A_UN_PROFESOR_TEXT));
+        talkNowPage.getPopupPreguntaInstructionsText().shouldBe(visible, exactText(PREGUNTE_POPUP_INSTRUCTIONS_TEXT));
+        talkNowPage.getPopupPreguntaMessageBlock().shouldBe(visible, empty,
+                attribute(PLACEHOLDER, PREGUNTE_POPUP_MESSAGE_PLACEHOLDER_TEXT));
+        talkNowPage.getPopupPreguntaEnviarButton().shouldBe(visible, exactText(ASK_TEACHER_SUBMIT_TEXT),
+                attribute(CLASS, ORANGE_BTN_DISABLED));
+        talkNowPage.getPopupPreguntaMessageBlock().sendKeys(SOME_TEXT_FOR_TEST);
+        talkNowPage.getPopupPreguntaEnviarButton().shouldBe(attribute(CLASS, ORANGE_BTN)).click();
+        talkNowPage.getPopupGraciasTitle().shouldBe(visible, exactText(ALLERT_HEADER_TEXT));
+        talkNowPage.getPopupGraciasMessageBlock().shouldBe(visible, exactText(PREGUNTE_POPUP_INSTRUCTIONS_TEXT));
+        talkNowPage.getPopupGraciasCerrarButton().shouldBe(visible, exactText(ALLERT_CERRAR_TEXT),
+                attribute(CLASS, ORANGE_BTN_CLOSE)).click();
+        talkNowPage.getHeaderText().shouldHave(text(TALK_NOW_HEADER_TEXT));
+    }
+
+}
