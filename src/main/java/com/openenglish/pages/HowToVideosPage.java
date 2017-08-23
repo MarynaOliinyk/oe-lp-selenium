9adb99aca1da57970e867afbe21ef1e502572d0f
Maryna Oliinyk <oliinyk.maryna@gmail.com>
2017-08-23 15:45:09 +0300

    Merge branch 'master' of https://github.com/openenglish/oe-lp-selenium into qaa-134/c44-verify-videos
    # Conflicts:
    #	src/main/java/com/openenglish/core/Attribute.java

--- src/main/java/com/openenglish/pages/HowToVideosPage.java
+++ src/main/java/com/openenglish/pages/HowToVideosPage.java
@@ -12,10 +12,7 @@ public class HowToVideosPage {
             video = $(By.id("video1")),
             titleOnVideos = $(By.xpath(".//*[@id='row1']/div[1]/p[1]")),
             descriptionOnVideo = $(By.xpath(".//*[@id='row1']/div[1]/p[2]")),
-            popUpVideo = $(By.id("video-popup")),
-            popUpControlPanel = $(By.xpath(".//*[@id='video-popup']/div[2]/div[3]")),
-            playButtonOnPopUp = $(By.xpath(".//*[@class='play-btn']")),
-            pauseButtonPopUp = $(By.xpath(".//*[@class='pause-btn']")),
             backwardButtonPopUp = $(By.xpath(".//*[@class='skip-back-btn']")),
-            forwardButtonPopUp = $(By.xpath(".//*[@class='skip-fwd-btn']"));
+            forwardButtonPopUp = $(By.xpath(".//*[@class='skip-fwd-btn']")),
+            bufferVideo = $(By.xpath(".//*[@class='current-time']"));
 }
