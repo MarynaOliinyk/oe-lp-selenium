7eb6009ef5d7b56d2cd939e5f9400d8dc9db6301
Maryna Oliinyk <oliinyk.maryna@gmail.com>
2017-08-23 19:09:32 +0300

    [QAA-137] C47 Verify links

--- src/main/java/com/openenglish/pages/TestYourSystemPage.java
+++ src/main/java/com/openenglish/pages/TestYourSystemPage.java
@@ -6,12 +6,18 @@ import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Selenide.$;
 
+@Getter
 public class TestYourSystemPage {
 
-    @Getter
     private SelenideElement headerText = $(By.xpath(".//*[@class='title-bar']//h1")),
             systemRequirements = $(By.xpath(".//*[@id='system-requirements']/span")),
             browserRequirements = $(By.xpath(".//*[@id='browser-requirements']/span")),
             otherRequirements = $(By.xpath(".//*[@id='other-requirements']/span")),
-            verificarTuSistemaButton = $(By.id("test-button"));
+            verificarTuSistemaButton = $(By.id("test-button")),
+            windowsLinkOnOtros = $(By.xpath(".//*[@data-device='pc']")),
+            popUpConfiguration = $(By.xpath(".//*[@id='instructions']/div[2]/img")),
+            closePopupButton = $(By.xpath(".//*[@class='ui-icon ui-icon-closethick']")),
+            verificacionDeEquipoLinkOnOtros = $(By.id("checkEquipment")),
+            macLinkOnOtros = $(By.xpath(".//*[@data-device='mac']"));
+    ;
 }
