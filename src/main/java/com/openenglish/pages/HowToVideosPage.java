0b9d6e1ab99e30fa5fb6816760610c310db5bbdb
Maryna Oliinyk <oliinyk.maryna@gmail.com>
2017-08-23 15:50:33 +0300

    [QAA-135] C45 Verify "Verifica tu sistema" content. Fix C44 Verify videos

--- src/main/java/com/openenglish/pages/HowToVideosPage.java
+++ src/main/java/com/openenglish/pages/HowToVideosPage.java
@@ -6,17 +6,14 @@ import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Selenide.$;
 
+@Getter
 public class HowToVideosPage {
-    @Getter
     private SelenideElement image = $(By.xpath(".//*[@class='heroShot']")),
             videoContent = $(By.id("row1")),
             video = $(By.id("video1")),
             titleOnVideos = $(By.xpath(".//*[@id='row1']/div[1]/p[1]")),
             descriptionOnVideo = $(By.xpath(".//*[@id='row1']/div[1]/p[2]")),
-            popUpVideo = $(By.id("video-popup")),
-            popUpControlPanel = $(By.xpath(".//*[@id='video-popup']/div[2]/div[3]")),
-            playButtonOnPopUp = $(By.xpath(".//*[@class='play-btn']")),
-    pauseButtonPopUp = $(By.xpath(".//*[@class='pause-btn']")),
-    backwardButtonPopUp = $(By.xpath(".//*[@class='skip-back-btn']")),
-    forwardButtonPopUp = $(By.xpath(".//*[@class='skip-fwd-btn']"));
+            backwardButtonPopUp = $(By.xpath(".//*[@class='skip-back-btn']")),
+            forwardButtonPopUp = $(By.xpath(".//*[@class='skip-fwd-btn']")),
+            bufferVideo = $(By.xpath(".//*[@class='current-time']"));
 }
