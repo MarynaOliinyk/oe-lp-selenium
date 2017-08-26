d307f6d9ae342dc79f716b3893f6d65504897866
Maryna Oliinyk <oliinyk.maryna@gmail.com>
2017-08-26 17:22:39 +0300

    [QAA-139] C49 Verify links and content

--- src/main/java/com/openenglish/pages/MeetingPage.java
+++ src/main/java/com/openenglish/pages/MeetingPage.java
@@ -7,5 +7,7 @@ import org.openqa.selenium.By;
 import static com.codeborne.selenide.Selenide.$;
 @Getter
 public class MeetingPage {
-    private SelenideElement text = $(By.xpath(".//*[@id='gecko']/h1"));
+    private SelenideElement text = $(By.xpath(".//*[@id='gecko']/h1")),
+    headerTestText = $(By.xpath(".//*[@id='mainLogo']/h1/a")),
+    adobeConnectTest =  $(By.xpath(".//*[@id='test']/embed"));
 }
---
+++ src/test/java/com/openenglish/ayuda/verifiqueSuConfiguracion/C49VerifyLinkAndContentTest.java
@@ -0,0 +1,35 @@
+package com.openenglish.ayuda.verifiqueSuConfiguracion;
+
+import com.openenglish.core.DriverBase;
+import com.openenglish.pages.AbstractPage;
+import com.openenglish.pages.MeetingPage;
+import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.switchTo;
+import static com.openenglish.core.TestData.General.LP_URL;
+import static com.openenglish.core.TestData.Login.CORRECT_PASSWORD;
+import static com.openenglish.core.TestData.Login.REGISTERED_EMAIL;
+import static com.openenglish.core.TestData.Meeting.MEETING_HEADER_TEST_TEXT;
+import static com.openenglish.core.TestData.Meeting.MEETING_TEST_TEXT;
+
+public class C49VerifyLinkAndContentTest extends DriverBase {
+
+    private AbstractPage page = new AbstractPage();
+    private MeetingPage meetingPage = new MeetingPage();
+
+    @Test
+    public void verifyLinkAndContentTest() {
+        open(LP_URL);
+        page.loginPage.cookieBannerVisibility();
+        page.loginPage.logIn(REGISTERED_EMAIL, CORRECT_PASSWORD);
+        page.inicioPage.getAyudaDropDown().click();
+        page.inicioPage.getVerifiqueSuConfiguracion().click();
+        switchTo().window(1);
+        meetingPage.getText().should(text(MEETING_TEST_TEXT));
+        meetingPage.getHeaderTestText().should(text(MEETING_HEADER_TEST_TEXT));
+        meetingPage.getAdobeConnectTest().should(visible);
+    }
+}
