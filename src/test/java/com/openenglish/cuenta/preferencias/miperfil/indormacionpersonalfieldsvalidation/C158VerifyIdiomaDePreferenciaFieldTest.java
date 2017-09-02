5bd6e4aa2f7cbbfb807d9266bdee40c46ec3e66e
Anton <kotuyka@gmail.com>
2017-09-02 14:13:25 +0300

    code refactoring

--- src/test/java/com/openenglish/cuenta/preferencias/miperfil/indormacionpersonalfieldsvalidation/C158VerifyIdiomaDePreferenciaFieldTest.java
+++ src/test/java/com/openenglish/cuenta/preferencias/miperfil/indormacionpersonalfieldsvalidation/C158VerifyIdiomaDePreferenciaFieldTest.java
@@ -1,29 +1,23 @@
 package com.openenglish.cuenta.preferencias.miperfil.indormacionpersonalfieldsvalidation;
 
 import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
 import com.openenglish.core.DriverBase;
 import com.openenglish.pages.InicioPage;
 import com.openenglish.pages.LoginPage;
 import com.openenglish.pages.PreferencesPage;
 import org.testng.annotations.Test;
 
-import static com.codeborne.selenide.Condition.attribute;
 import static com.codeborne.selenide.Condition.empty;
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.not;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.open;
-import static com.openenglish.core.Attribute.TYPE;
 import static com.openenglish.core.TestData.General.LP_URL;
 import static com.openenglish.core.TestData.Login.CORRECT_PASSWORD;
 import static com.openenglish.core.TestData.Login.REGISTERED_EMAIL;
-import static com.openenglish.core.TestData.Preferences.PREFERENCES_CONFIRM_NEW_PASSWORD_NOTIFICATION;
 import static com.openenglish.core.TestData.Preferences.PREFERENCES_MI_PERFIL_TAB_TEXT;
-import static com.openenglish.core.TestData.Preferences.PREFERENCES_NEW_PASSWORD_VALID;
-import static com.openenglish.core.TestData.Preferences.PREFERENCES_PASSWORDS_NOT_EQUALS_NOTIFICATION;
-import static com.openenglish.core.TestData.Preferences.PREFERENCES_PASSWORD_TEXT;
-import static com.openenglish.core.TestData.Preferences.PREFERENCES_SECOND_NEW_PASSWORD_VALID;
 import static org.testng.Assert.assertTrue;
 
 public class C158VerifyIdiomaDePreferenciaFieldTest extends DriverBase {
@@ -43,9 +37,9 @@ public class C158VerifyIdiomaDePreferenciaFieldTest extends DriverBase {
         pp.getIdiomaDePreferenciaField().shouldBe(visible).click();
         ElementsCollection idiomasList = pp.getAllTheIdiomasList();
         assertTrue(idiomasList.stream().allMatch(element -> element.is(not(empty))));
-        String firsrIdioma = idiomasList.stream().findFirst().get().getText();
-        idiomasList.stream().findFirst().get().click();
-        pp.getIdiomaDePreferenciaField().shouldHave(text(firsrIdioma));
+        SelenideElement firstIdioma = idiomasList.stream().findFirst().get();
+        firstIdioma.click();
+        pp.getIdiomaDePreferenciaField().shouldHave(text(firstIdioma.getText()));
     }
 
 }
